86a83f6ea9f3fbec792c444b12951a3024556df9
Anusha Syeda <anoshasyeda1999@gmail.com>
2024-09-07 00:05:29 +0500

    chore: config invariant

--- src/component/Card/Card.tsx
+++ src/component/Card/Card.tsx
@@ -1,5 +1,9 @@
 //* React Import
-import React from "react";
+import { useEffect, useRef } from "react";
+
+//* Pragmatic & other Imports
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
+import invariant from "tiny-invariant";
 
 //* CardProps Import
 import { CardContentProps } from "@/interface/interfaces";
@@ -10,9 +14,19 @@ interface CardProps {
 }
 
 export const Card = ({ Content }: CardProps) => {
+  //* use Hooks
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    invariant(el);
+    return draggable({ element: el });
+  }, []);
+
   return (
     <div
       aria-label="card"
+      ref={ref}
       className="border-2 w-fit border-dotted rounded-md border-cyan-600 cursor-grab p-2.5"
     >
       <section aria-label="card-title" className="text-[1.5rem]">
